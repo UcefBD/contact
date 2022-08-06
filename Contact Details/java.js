c45d465759ef4e918fb5ffca51addc0b3e9b6d4a
Ucef <72914117+UcefBD@users.noreply.github.com>
2022-08-06 18:23:20 +0300

    Add files via upload

---
+++ Contact Details/java.js
@@ -0,0 +1,13 @@
+var phonebook= new Array();
+var myFunction = function(){
+
+  var Phone = document.getElementById("Phonenumber").value;
+  
+  var Name  = 
+      document.getElementById("Firstname").value;
+  document.getElementById("Phone2").innerHTML.value;
+ phonebook.push(Name +" "+Phone +"</br>");
+document.write(phonebook);  
+}
+
+
